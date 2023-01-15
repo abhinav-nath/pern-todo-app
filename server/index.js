b92414c517a1e0a9c6d5daa64c4d391b537f2a8b
Abhinav Nath <abhinavnath@ymail.com>
2023-01-16 00:03:53 +0530

    [server] Add code to connect to DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// routes
 
 app.listen(5001, () => {
   console.log("server has started on port 5001");
