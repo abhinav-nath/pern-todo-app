0c783d2ee72eddb9d381bc030ba3d14a8593f71d
Abhinav Nath <abhinavnath@ymail.com>
2023-01-16 00:22:59 +0530

    [server] Add  a POST endpoint to create a todo

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ app.use(express.json());
 
 // routes
 
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 app.listen(5001, () => {
   console.log("server has started on port 5001");
 });
